0c52712ffb37c2e2acb66a8fb62b60e544eca49c
sumitjambharkar <sujam3545@gmail.com>
2024-04-25 20:33:27 +0530

    commit

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -3,9 +3,9 @@ import {NextResponse,NextRequest} from "next/server";
 import User from "@/model/User";
 
 
-export async function GET (req:NextRequest) {
+export async function GET (req) {
     try {
-       const userId = await getTokenData(NextRequest);
+       const userId = await getTokenData(req.cookies);
        const user = await User.findById({_id:userId}).select("-password");
        return NextResponse.json({message:"User Found",data:user})
     } catch (error) {
