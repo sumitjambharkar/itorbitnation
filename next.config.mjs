8ade44583aaf1eac6741fa379c4d7c5e529a7bed
sumitjambharkar <sujam3545@gmail.com>
2024-05-09 21:47:27 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['res.cloudinary.com'],
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  redirects: async () => {
+    return [
+      {
+        source: "/blog/digital-marketing-services-in-andheri",
+        destination: "/",
+        permanent: false,
       },
+    ];
+  },
 };
 
 export default nextConfig;
