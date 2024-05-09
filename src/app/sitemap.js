7ed8683eb380488b08f7d731950f352465babf45
sumitjambharkar <sujam3545@gmail.com>
2024-05-09 22:19:44 +0530

    commit

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -4,70 +4,70 @@ export default async function sitemap() {
     return [{
         
         url:"https://itorbitnation.com/",
-        lastModified:'2024-04-27T14:10:56+00:0',
+        lastModified:new Date(),
     },{
         
         url:"https://itorbitnation.com/about",
-        lastModified:'2024-04-27T14:10:56+00:0',
+        lastModified:new Date(),
     },{
         
         url:"https://itorbitnation.com/service",
-        lastModified:'2024-04-27T14:10:56+00:0',
+        lastModified:new Date(),
     }
     ,{
         
         url:"https://itorbitnation.com/course",
-        lastModified:'2024-04-27T14:10:56+00:0',
+        lastModified:new Date(),
     }
     ,{
         
         url:"https://itorbitnation.com/freetech",
-        lastModified:'2024-04-27T14:10:56+00:0',
+        lastModified:new Date(),
     }
     ,{
         
         url:"https://itorbitnation.com/blog",
-        lastModified:'2024-04-27T14:10:56+00:0',
+        lastModified:new Date(),
     }
     ,{
         
         url:"https://itorbitnation.com/contact",
-        lastModified:'2024-04-27T14:10:56+00:0',
+        lastModified:new Date(),
     }
     ,{
         
         url:"https://itorbitnation.com/signup",
-        lastModified:'2024-04-27T14:10:56+00:0',
+        lastModified:new Date(),
     }
     ,{
         
         url:"https://itorbitnation.com/inquiry",
-        lastModified:'2024-04-27T14:10:56+00:0',
+        lastModified:new Date(),
     }
     ,{
         
         url:"https://itorbitnation.com/resume",
-        lastModified:'2024-04-27T14:10:56+00:0',
+        lastModified:new Date(),
     }
     ,{
         
         url:"https://itorbitnation.com/inquiry/student",
-        lastModified:'2024-04-27T14:10:56+00:0',
+        lastModified:new Date(),
     }
     ,{
         
         url:"https://itorbitnation.com/inquiry/services",
-        lastModified:'2024-04-27T14:10:56+00:0',
+        lastModified:new Date(),
     }
     ,{
         
         url:"https://itorbitnation.com/blog/digital-marketing-services-in-andheri",
-        lastModified:'2024-04-27T14:10:56+00:0',
+        lastModified:new Date(),
     }
     ,{
         
         url:"https://itorbitnation.com/blog/digital-marketing-courses-in-andheri",
-        lastModified:'2024-04-27T14:10:56+00:0',
+        lastModified:new Date(),
     }
 ]
 }
